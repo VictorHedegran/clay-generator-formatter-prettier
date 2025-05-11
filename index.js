2b8afbca730b6338235e9d77b954e4bbed4fbfa1
Victor Hedegran <victor.hedegran@gmail.com>
2025-05-11 10:16:35 +0200

    add support to prettify vue/nuxt projects

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const prettier = require('prettier');
 module.exports = {
-    extensions: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+    extensions: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx', '**/*.vue'],
     apply: async function(file_path, content) {
         const options = await  prettier.resolveConfig(file_path);
         return prettier.format(content, {filepath: file_path, ...options});
