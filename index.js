58effed2a6c1088b5ebcca730ef2b53f44522cbb
Victor Hedegran <victor.hedegran@gmail.com>
2025-10-04 19:21:23 +0200

    remove @clay comment added when using syntax highlighting extension

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const prettier = require('prettier');
 module.exports = {
     extensions: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
     apply: async function(file_path, content) {
+            const cleanContent = content.replace(/^\/\/ @clay\s*\n?/, "");
+
         const options = await  prettier.resolveConfig(file_path);
-        return prettier.format(content, {filepath: file_path, ...options});
+        return prettier.format(cleanContent, {filepath: file_path, ...options});
     }
 }
