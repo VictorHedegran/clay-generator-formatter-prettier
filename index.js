03e68fb7189d3ae58a59855c6872b83c0593561c
Victor Hedegran <victor.hedegran@gmail.com>
2025-10-04 20:19:12 +0200

    manual formatting

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const prettier = require('prettier');
 module.exports = {
     extensions: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
     apply: async function(file_path, content) {
-            const cleanContent = content.replace(/^\/\/ @clay\s*\n?/, "");
-
+        const cleanContent = content.replace(/^\/\/ @clay\s*\n?/, "");
         const options = await  prettier.resolveConfig(file_path);
         return prettier.format(cleanContent, {filepath: file_path, ...options});
     }
